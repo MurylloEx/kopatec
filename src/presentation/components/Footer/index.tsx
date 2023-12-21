cb0a52dc7ec4227c7c9f62ba32a72a9e20370865
MurylloEx <32225687+MurylloEx@users.noreply.github.com>
2023-12-20 22:14:21 -0300

    feat: footer component

---
+++ src/presentation/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+import { Styled } from './styles';
+
+export interface FooterProps {}
+
+export const Footer: FunctionComponent<FooterProps> = (props) => {
+  return (
+    <Styled.Container>
+
+    </Styled.Container>
+  );
+}
+
